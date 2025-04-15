0f235573b04ca5e1e9762747a269768694068045
anushka-udayanga-2002 <a.udayange2002@gmail.com>
2025-04-15 20:33:00 +0530

    Update app.py

--- youtube_downloader/app.py
+++ youtube_downloader/app.py
@@ -4,6 +4,10 @@ from pytube import YouTube
 import os
 import uuid
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 app = Flask(__name__)
 
 @app.route('/')
