1d24b0e19326171b0c0c79c0735a78fa072cefc4
anushka-udayanga-2002 <a.udayange2002@gmail.com>
2025-04-15 20:37:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, request, render_template, send_file
+from pytube import YouTube
+import os
+import uuid
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_streams', methods=['POST'])
+def get_streams():
+    url = request.form['url']
+    yt = YouTube(url)
+    streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+    audio_stream = yt.streams.filter(only_audio=True).first()
+
+    return render_template('index.html', url=url, streams=streams, audio_stream=audio_stream, title=yt.title)
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    stream_itag = request.form['itag']
+    yt = YouTube(url)
+    stream = yt.streams.get_by_itag(stream_itag)
+    
+    filename = f"{uuid.uuid4()}.mp4" if "video" in stream.mime_type else f"{uuid.uuid4()}.mp3"
+    filepath = stream.download(filename=filename)
+
+    return send_file(filepath, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
