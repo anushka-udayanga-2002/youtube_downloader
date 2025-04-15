0aa392d136b971c5442b3737f1ed2a49005dbc1e
anushka-udayanga-2002 <a.udayange2002@gmail.com>
2025-04-15 20:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from pytube import YouTube
 import os
 import uuid
 
+app = Flask(__name__)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
-    
-app = Flask(__name__)
+
 
 @app.route('/')
 def index():
